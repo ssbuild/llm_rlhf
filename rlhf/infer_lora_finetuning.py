98eb166fb1c7c8275a0d00b17f13ddb52212207f
ssbuild <462304@qq.cn>
2023-05-11 12:28:19 +0800

    add infer_lora
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ rlhf/infer_lora_finetuning.py
@@ -0,0 +1,50 @@
+# @Time    : 2023/4/2 22:49
+# @Author  : tk
+# @FileName: infer_lora_finetuning
+import os
+
+import torch
+from deep_training.data_helper import ModelArguments, TrainingArguments, DataArguments
+from transformers import HfArgumentParser,AutoConfig,PreTrainedTokenizer
+
+from data_utils import train_info_args, NN_DataHelper
+from models import MyPPOTransformer, Generate, load_in_8bit,LoraArguments,PPOArguments
+
+if __name__ == '__main__':
+    train_info_args['seed'] = None
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments, LoraArguments,PPOArguments))
+    model_args, training_args, data_args, _,_ = parser.parse_dict(train_info_args)
+
+
+
+    dataHelper = NN_DataHelper(model_args, training_args, data_args)
+    tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config()
+
+    ckpt_dir = './best_ckpt'
+    config = AutoConfig.from_pretrained(ckpt_dir)
+    lora_args = LoraArguments.from_pretrained(ckpt_dir)
+
+    assert lora_args.inference_mode == True
+
+    pl_model = MyPPOTransformer(config=config, model_args=model_args, training_args=training_args,lora_args=lora_args)
+    # 加载lora权重
+    pl_model.backbone.from_pretrained(pl_model.backbone.model, pretrained_model_name_or_path=ckpt_dir, lora_config = lora_args)
+    if load_in_8bit:
+        pl_model.eval().cuda()
+    else:
+        pl_model.eval().half().cuda()
+
+    enable_merge_weight = False
+
+    if enable_merge_weight:
+        # 合并lora 权重 保存
+        pl_model.save_pretrained_merge_lora(os.path.join(ckpt_dir, 'pytorch_model_merge.bin'))
+    else:
+        model = pl_model.get_llm_model()
+
+        text = "哪些食物对糖尿病患者有好处?"
+        response, history = Generate.chat(model, query=text, tokenizer=tokenizer, max_length=512,
+                                          eos_token_id=config.eos_token_id,
+                                          do_sample=True, top_p=0.7, temperature=0.95, )
+        print('input', text)
+        print('output', response)
