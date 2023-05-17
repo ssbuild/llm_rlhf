633d6e238dd840ad71b725bf80a305922dcfbfac
ssbuild <462304@qq.cn>
2023-05-17 15:00:52 +0800

    evalute
    Signed-off-by: ssbuild <462304@qq.cn>

--- reward/evaluate_dev.py
+++ reward/evaluate_dev.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/5/17 11:36
-
+# @Time    : 2023/5/17 14:26
+import json
 import os
 import numpy as np
 import torch
 from deep_training.data_helper import ModelArguments, TrainingArguments, DataArguments
+from tqdm import tqdm
 from transformers import HfArgumentParser,AutoConfig,PreTrainedTokenizer
 
 from data_utils import train_info_args, NN_DataHelper
@@ -33,27 +34,28 @@ if __name__ == '__main__':
         pl_model.eval().cuda()
     else:
         pl_model.eval().half().cuda()
+    pl_model.requires_grad_(False)
 
-    enable_merge_weight = False
 
-    if enable_merge_weight:
-        # 合并lora 权重 保存
-        pl_model.save_pretrained_merge_lora(os.path.join(ckpt_dir, 'pytorch_model_merge.bin'))
-    else:
-
-        pl_model.requires_grad_(False)
-
+    with open('./data/eval.json',mode='r',encoding='utf-8') as f:
+        lines = f.readlines()
+    print('predict........')
+    acc = 0
+    total = 0
+    for line in tqdm(lines,total=len(lines)):
+        jd = json.loads(line)
+        if not jd:
+            continue
         input_list = [
-            "\n\nHuman:如何培养土豆\n\nAssistant:土豆生长在地下,然后发送的干子称为花生,这些花生成长为我们熟悉的土豆。",
-            "\n\nHuman:如何培养土豆\n\nAssistant:土豆在地下生长成大、坚固的花生,一旦土豆长大了,它们就生长在地上。",
-            "\n\nHuman:火柴是怎样制造的?\n\nAssistant:我猜你问我如何制造某些东西,但我们以前从未真正讨论过制造的细节。",
-            "\n\nHuman:火柴是怎样制造的?\n\nAssistant:对不起,我担心我不明白你的问题。",
+            jd['prompt'][:256] + jd['chosen'][:256],
+            jd['prompt'][:256] + jd['rejected'][:256],
         ]
-        input_list = [_[:256] for _ in input_list]
         tokend = tokenizer(input_list,padding=True,truncation=True)
         input_ids = torch.tensor(tokend["input_ids"],dtype=torch.int32).to(pl_model.device)
         output = pl_model.backbone.compute_loss(input_ids=input_ids)
         _,scores = output
+        total += 1
+        if scores[0] >= scores[1]:
+           acc += 1
 
-        for text,score in zip(input_list,scores):
-            print('score:' ,score, "text ",text.replace('\n',''))
+    print('total {} , acc count {} , acc {}'.format(total,acc, acc / total))
