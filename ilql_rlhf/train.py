fix precision
Signed-off-by: ssbuild <462304@qq.cn>

ilql_rlhf/train.py
@@ -80,7 +80,8 @@ if __name__ == '__main__':
         accumulate_grad_batches=training_args.gradient_accumulation_steps,
         #max_grad_norm=training_args.max_grad_norm,
         strategy=strategy,
-        precision='16',  #  "32": "32-true", "16": "16-mixed", "bf16": "bf16-mixed"
+        # lora int8 precision='32'
+        precision='32' if global_args['load_in_8bit'] else '16',# 可以自行尝试  "32": "32-true", "16": "16-mixed", "bf16": "bf16-mixed"
     )
     dataHelper = NN_DataHelper(model_args, training_args, data_args,ilql_args=ilql_args)
     config_kwargs = {"torch_dtype": torch.float16}