9a304b9c24d299d698899c0dc6c52aac34f421a5
ssbuild <462304@qq.com>
2023-05-28 00:35:18 +0800

    0.1.9
    Signed-off-by: ssbuild <462304@qq.com>

--- stage3_ppo/train.py
+++ stage3_ppo/train.py
@@ -53,6 +53,33 @@ if __name__ == '__main__':
     lora_args = lora_args.config
     ppo_args = ppo_args.config
 
+    dataHelper = NN_DataHelper(model_args, training_args, data_args, ppo_args=ppo_args, rrhf_weight=1.0,
+                               length_penalty=1.0)
+    config_kwargs = {"torch_dtype": torch.float16}
+    if global_args["num_layers"] > 0:
+        config_kwargs[global_args["num_layers_key"]] = global_args["num_layers"]
+    tokenizer, config, _, _ = dataHelper.load_tokenizer_and_config(config_kwargs=config_kwargs)
+    config.decoder_start_token_id = config.bos_token_id
+
+    if "llama" in model_args.model_name_or_path.lower() and tokenizer.bos_token_id != DEFAULT_BOS_TOKEN:
+        tokenizer.add_special_tokens({
+            "eos_token": DEFAULT_EOS_TOKEN,
+            "bos_token": DEFAULT_BOS_TOKEN,
+            "unk_token": DEFAULT_UNK_TOKEN,
+        })
+        if tokenizer.pad_token_id is None or tokenizer.pad_token_id == -1:
+            tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    config.save_pretrained('best_ckpt')
+
+    # 缓存数据集
+    if data_args.do_train:
+        dataHelper.make_dataset_with_args(data_args.train_file, mixed_data=False, shuffle=True, mode='train')
+    if data_args.do_eval:
+        dataHelper.make_dataset_with_args(data_args.eval_file, mode='eval')
+    if data_args.do_test:
+        dataHelper.make_dataset_with_args(data_args.test_file, mode='test')
+
     checkpoint_callback = MySimpleModelCheckpoint(
         # monitor="loss",
         save_weights_only=True,
@@ -78,41 +105,16 @@ if __name__ == '__main__':
         accumulate_grad_batches=training_args.gradient_accumulation_steps,
         #max_grad_norm=training_args.max_grad_norm,
         strategy=strategy,
-        # lora int8 precision='32'
-        precision='32' if global_args['load_in_8bit'] else '16',# 可以自行尝试  "32": "32-true", "16": "16-mixed", "bf16": "bf16-mixed"
+        precision='32',# 可以自行尝试  "32": "32-true", "16": "16-mixed", "bf16": "bf16-mixed"
     )
 
-
-    dataHelper = NN_DataHelper(model_args, training_args, data_args,ppo_args=ppo_args,rrhf_weight=1.0,length_penalty=1.0)
-    config_kwargs = {"torch_dtype": torch.float16}
-    if global_args["num_layers"] > 0:
-        config_kwargs[global_args["num_layers_key"]] = global_args["num_layers"]
-    tokenizer, config, _, _ = dataHelper.load_tokenizer_and_config(config_kwargs=config_kwargs)
-    config.decoder_start_token_id = config.bos_token_id
-
-    if "llama" in model_args.model_name_or_path.lower() and tokenizer.bos_token_id != DEFAULT_BOS_TOKEN:
-        tokenizer.add_special_tokens({
-            "eos_token": DEFAULT_EOS_TOKEN,
-            "bos_token": DEFAULT_BOS_TOKEN,
-            "unk_token": DEFAULT_UNK_TOKEN,
-        })
-        if tokenizer.pad_token_id is None or tokenizer.pad_token_id == -1:
-            tokenizer.pad_token_id = tokenizer.eos_token_id
 
 
     # 额外参数
     # checkpoint_callback.tokenizer = tokenizer
     # checkpoint_callback.data_args = data_args
 
-    config.save_pretrained('best_ckpt')
 
-    # 缓存数据集
-    if data_args.do_train:
-        dataHelper.make_dataset_with_args(data_args.train_file, mixed_data=False, shuffle=True, mode='train')
-    if data_args.do_eval:
-        dataHelper.make_dataset_with_args(data_args.eval_file, mode='eval')
-    if data_args.do_test:
-        dataHelper.make_dataset_with_args(data_args.test_file, mode='test')
 
     if trainer.global_rank == 0:
         pl_reward_model = load_reward_model('../stage2_reward/best_ckpt')
@@ -156,7 +158,10 @@ if __name__ == '__main__':
 
 
     pl_model = MyPPOTransformer(config=config,model_args=model_args,training_args=training_args,lora_args=lora_args,ppo_args=ppo_args,
-                                load_in_8bit=global_args["load_in_8bit"],device_map={"": trainer.fabric.local_rank} if trainer.world_size > 1 else "auto")
+                                quantization_config=global_args["quantization_config"],
+                                load_in_8bit=global_args["load_in_8bit"],
+                                device_map={"": trainer.local_rank} if trainer.world_size > 1 else "auto",
+                                torch_dtype=torch.float16, )
 
     # 加载 sft 权重
     # pl_model.load_sft_weight('sft_weight.bin',is_trainable=True)
--- stage3_rrhf/train.py
+++ stage3_rrhf/train.py
@@ -45,6 +45,31 @@ if __name__ == '__main__':
     model_args, training_args, data_args, lora_args = parser.parse_dict(train_info_args)
     lora_args = lora_args.config
 
+    dataHelper = NN_DataHelper(model_args, training_args, data_args)
+    config_kwargs = {"torch_dtype": torch.float16}
+    if global_args["num_layers"] > 0:
+        config_kwargs[global_args["num_layers_key"]] = global_args["num_layers"]
+    tokenizer, config, _, _ = dataHelper.load_tokenizer_and_config(config_kwargs=config_kwargs)
+    config.decoder_start_token_id = config.bos_token_id
+
+    if "llama" in model_args.model_name_or_path.lower() and tokenizer.bos_token_id != DEFAULT_BOS_TOKEN:
+        tokenizer.add_special_tokens({
+            "eos_token": DEFAULT_EOS_TOKEN,
+            "bos_token": DEFAULT_BOS_TOKEN,
+            "unk_token": DEFAULT_UNK_TOKEN,
+        })
+        if tokenizer.pad_token_id is None or tokenizer.pad_token_id == -1:
+            tokenizer.pad_token_id = tokenizer.eos_token_id
+    config.save_pretrained('best_ckpt')
+
+    # 缓存数据集
+    if data_args.do_train:
+        dataHelper.make_dataset_with_args(data_args.train_file, mixed_data=False, shuffle=True, mode='train')
+    if data_args.do_eval:
+        dataHelper.make_dataset_with_args(data_args.eval_file, mode='eval')
+    if data_args.do_test:
+        dataHelper.make_dataset_with_args(data_args.test_file, mode='test')
+
     deepspeed_config = get_deepspeed_config()
     # 保存最小loss模型
     if lora_args is not None:
@@ -85,43 +110,20 @@ if __name__ == '__main__':
         accumulate_grad_batches=training_args.gradient_accumulation_steps,
         num_sanity_val_steps=0,
         strategy=strategy,
-        # lora int8 precision='32'
-        precision='32' if global_args['load_in_8bit'] else '16',# 可以自行尝试  "32": "32-true", "16": "16-mixed", "bf16": "bf16-mixed"
+        precision='32',# 可以自行尝试  "32": "32-true", "16": "16-mixed", "bf16": "bf16-mixed"
     )
-
-
-    dataHelper = NN_DataHelper(model_args, training_args, data_args)
-    config_kwargs = {"torch_dtype": torch.float16}
-    if global_args["num_layers"] > 0:
-        config_kwargs[global_args["num_layers_key"]] = global_args["num_layers"]
-    tokenizer, config, _, _ = dataHelper.load_tokenizer_and_config(config_kwargs=config_kwargs)
-    config.decoder_start_token_id = config.bos_token_id
-
-    if "llama" in model_args.model_name_or_path.lower() and tokenizer.bos_token_id != DEFAULT_BOS_TOKEN:
-        tokenizer.add_special_tokens({
-            "eos_token": DEFAULT_EOS_TOKEN,
-            "bos_token": DEFAULT_BOS_TOKEN,
-            "unk_token": DEFAULT_UNK_TOKEN,
-        })
-        if tokenizer.pad_token_id is None or tokenizer.pad_token_id == -1:
-            tokenizer.pad_token_id = tokenizer.eos_token_id
 
     # 额外参数
     checkpoint_callback.tokenizer = tokenizer
     checkpoint_callback.data_args = data_args
 
-    config.save_pretrained('best_ckpt')
 
-    # 缓存数据集
-    if data_args.do_train:
-        dataHelper.make_dataset_with_args(data_args.train_file, mixed_data=False, shuffle=True, mode='train')
-    if data_args.do_eval:
-        dataHelper.make_dataset_with_args(data_args.eval_file, mode='eval')
-    if data_args.do_test:
-        dataHelper.make_dataset_with_args(data_args.test_file, mode='test')
 
     pl_model = MyRRHFTransformer(config=config, model_args=model_args, training_args=training_args, lora_args=lora_args,
-                                   load_in_8bit=global_args["load_in_8bit"],device_map={"": trainer.local_rank} if trainer.world_size > 1 else "auto")
+                                 quantization_config=global_args["quantization_config"],
+                                 load_in_8bit=global_args["load_in_8bit"],
+                                 device_map={"": trainer.local_rank} if trainer.world_size > 1 else "auto",
+                                 torch_dtype=torch.float16, )
 
     # 加载sft权重训练
     # pl_model.load_sft_weight('sft_weight.bin',is_trainable=True)
