fa2b91c6f0637f18e5069c53bc3bb2de4ee5a96b
ssbuild <462304@qq.cn>
2023-05-29 13:27:24 +0800

    fix deepspeed load weight at score module
    Signed-off-by: ssbuild <462304@qq.cn>

--- models/ilql_model.py
+++ models/ilql_model.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/5/18 16:41
+# @Author  : ssbuild
+# @Time    : 2023/5/29 9:52
 import torch
-from deep_training.nlp.models.transformer import TransformerForCausalLM
-from torch.nn import functional as F
+from deep_training.nlp.rl.ilql.configuration import ILQLArguments, ILQLConfig
+from deep_training.nlp.rl.ilql.ilql_module import ILQLModelLoss
+from deep_training.nlp.models.rl.modeling_ilql import AutoModelForCausalLMWithILQLHeads
+from transformers import AdamW
 from models.model_weight import *
 
 
-__all__ = [
-    'RRHFModelForCausalLM'
-]
-
-class RRHFModelForCausalLM(TransformerForCausalLM):
-    def __init__(self,*args,**kwargs):
+class ILQLModelForCausalLMWithILQLHeads(AutoModelForCausalLMWithILQLHeads):
+    def __init__(self, *args,hidden_size=None, up_sampling_score=False,**kwargs):
+        config = kwargs.get('config')
+        if hidden_size is None:
+            hidden_size = config.word_embed_proj_dim if getattr(config, 'word_embed_proj_dim', None) else config.hidden_size
         # 如果显卡支持int8 可以开启
         load_in_8bit = kwargs.get('load_in_8bit', False)
         load_in_4bit = kwargs.get('load_in_4bit', False)
@@ -23,11 +25,7 @@ class RRHFModelForCausalLM(TransformerForCausalLM):
         if not load_in_8bit and not load_in_4bit:
             kwargs.pop("device_map", None)
             kwargs.pop("quantization_config", None)
-        super(RRHFModelForCausalLM, self).__init__(*args, **kwargs)
-
-
-        self.length_penalty = kwargs.get('length_penalty',1.0)
-        self.rrhf_weight = kwargs.get('rrhf_weight', 1.0)
+        super(ILQLModelForCausalLMWithILQLHeads, self).__init__(*args,hidden_size=hidden_size,up_sampling_score=up_sampling_score, **kwargs)
 
     def enable_input_require_grads(self):
         setattr(self.model, 'model_parallel', True)
@@ -35,63 +33,22 @@ class RRHFModelForCausalLM(TransformerForCausalLM):
         # self.model.gradient_checkpointing_enable()
         self.model.enable_input_require_grads()
 
-    def gather_logits_labels(self, logits, labels,mask):
-        new_logits = logits.clone()  # Create a copy to avoid in-place modification
-        labels[labels == -100] = 0
-        output = torch.gather(new_logits, dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
-        output = output * mask  # B * L
-        return output
 
-    def get_score(self, logit_label, mask):
-        length = mask.sum(-1)
-        scores = logit_label.sum(-1) / (length ** self.length_penalty)
-        return scores
-
-    def rrhf_loss(self, scores, rw_scores):
-        diff = scores.unsqueeze(0) - scores.unsqueeze(-1)  # b * b
-        rw_diff = rw_scores.unsqueeze(0) - rw_scores.unsqueeze(-1)  # b * b
-        aval = torch.bitwise_and(rw_diff > 0, diff < 0)[0]
-        return -diff[aval].sum()
-
-    def sft_loss(self, logit_label, rw_scores):
-        max_idx = torch.argmax(rw_scores)
-        return -logit_label[max_idx].mean()
-
-    def compute_loss(self, **inputs):
-        labels = inputs.pop('labels',None)
-        scores = inputs.pop('scores', None)
-        logits = self.model(**inputs)[0]  # (batch * cand) * L * V
-        if labels is not None:
-            labels = labels.long()
-            logits = F.log_softmax(logits, dim=-1)
-            mask = (labels != -100).float()
-            logit_label = self.gather_logits_labels(logits, labels,mask)
-            compute_scores = self.get_score(logit_label,mask)
-            rrhf_loss = self.rrhf_loss(compute_scores, scores)
-            sft_loss = self.sft_loss(logit_label, scores)
-            loss = self.rrhf_weight * rrhf_loss + sft_loss
-            loss_dict = {
-                "rrhf_loss": rrhf_loss,
-                "sft_loss": sft_loss,
-                "loss": loss
-            }
-            return (loss_dict,)
-        return (logits,)
-
-
-
-
-
-
-
-class MyRRHFTransformer(RRHFModelForCausalLM,SftWeightMinMax,with_pl=True):
+class MyILQLTransformer(ILQLModelForCausalLMWithILQLHeads, ILQLModelLoss,SftWeightMinMax, with_pl=True):
     def __init__(self, *args, **kwargs):
         lora_args: LoraConfig = kwargs.pop('lora_args', None)
         prompt_args: PromptLearningConfig = kwargs.pop('prompt_args', None)
-        super(MyRRHFTransformer, self).__init__(*args, **kwargs)
+        ilql_args: ILQLConfig = kwargs.pop('ilql_args', None)
+        if ilql_args is not None:
+            kwargs.update({
+                "two_qs": ilql_args.two_qs,
+                "alpha": ilql_args.alpha,
+            })
+        super(MyILQLTransformer, self).__init__(*args, **kwargs)
 
         self.lora_args = lora_args
-        self.prompt_args=prompt_args
+        self.ilql_config = ilql_args
+        self.prompt_args = prompt_args
         if lora_args is not None and lora_args.with_lora:
             self.backbone.enable_input_require_grads()
             model: LoraModel = LoraModel(self.backbone, lora_args, auto_prepare_kbit_training=False)
@@ -116,7 +73,7 @@ class MyRRHFTransformer(RRHFModelForCausalLM,SftWeightMinMax,with_pl=True):
             return [(self.backbone, lr)]
         elif self.prompt_args and self.prompt_args.with_prompt:
             return [(self.backbone, lr)]
-        return super(MyRRHFTransformer, self).get_model_lr(model, lr)
+        return super(MyILQLTransformer, self).get_model_lr(model, lr)
 
     def get_llm_model(self) -> PreTrainedModel:
         if self.lora_args is not None and self.lora_args.with_lora:
@@ -127,5 +84,30 @@ class MyRRHFTransformer(RRHFModelForCausalLM,SftWeightMinMax,with_pl=True):
         return self.backbone.model
 
     @torch.no_grad()
-    def generate(self, *args, **kwargs):
-        return self.get_llm_model().generate(*args, **kwargs)
+    def generate(self,*args,**kwargs):
+        return self.get_llm_model().generate(*args,**kwargs)
+
+    def configure_optimizers(self):
+        p = self.get_named_parameters(self.backbone)
+        training_args = self.training_args
+        optimizer = AdamW(p, lr=training_args.learning_rate,
+                          eps=training_args.adam_epsilon,
+                          betas=training_args.optimizer_betas,
+                          weight_decay=training_args.weight_decay)
+        return optimizer
+
+
+    def training_step(self,*args, **inputs):
+        outputs = self.compute_loss(*args, **inputs)
+        return outputs
+
+    def validation_step(self, batch):
+        outputs = self.compute_loss(**batch)
+        return outputs
+
+    def compute_loss(self, *args, **inputs):
+        return self.forward_ilql_loss(*args, **inputs)
+
+
+    def forward_logits_values(self,*args,**kwargs):
+        return self.model.forward(*args,**kwargs)
---
+++ models/ppo_model.py
@@ -0,0 +1,167 @@
+# coding=utf8
+# @Time    : 2023/5/12 21:31
+# @Author  : tk
+# @FileName: llm_model
+
+from typing import List, Tuple, Optional, Union
+import torch
+from deep_training.nlp.models.rl.modeling_ppo import AutoModelForCausalLMWithValueHead
+from deep_training.nlp.rl.ppo.configuration import PPOConfig
+from deep_training.nlp.rl.ppo.ppo_module import PPOModelLoss
+from transformers import AdamW
+from models.model_weight import *
+
+
+class PPOModelForCausalLMWithValueHead(AutoModelForCausalLMWithValueHead):
+    def __init__(self,*args,hidden_size=None, up_sampling_score=False,**kwargs):
+        config = kwargs.get('config')
+        if hidden_size is None:
+            hidden_size = config.word_embed_proj_dim if getattr(config, 'word_embed_proj_dim',
+                                                                None) else config.hidden_size
+        # 如果显卡支持int8 可以开启
+        load_in_8bit = kwargs.get('load_in_8bit', False)
+        load_in_4bit = kwargs.get('load_in_4bit', False)
+        if not load_in_4bit:
+            quantization_config = kwargs.get("quantization_config", None)
+            if quantization_config:
+                load_in_4bit = quantization_config.load_in_4bit
+
+        if not load_in_8bit and not load_in_4bit:
+            kwargs.pop("device_map", None)
+            kwargs.pop("quantization_config", None)
+
+        super(PPOModelForCausalLMWithValueHead, self).__init__(*args,hidden_size=hidden_size, up_sampling_score=up_sampling_score, **kwargs)
+
+
+
+    def enable_input_require_grads(self):
+        setattr(self.model, 'model_parallel', True)
+        setattr(self.model, 'is_parallelizable', True)
+        # self.model.gradient_checkpointing_enable()
+        self.model.enable_input_require_grads()
+
+
+
+
+
+
+
+
+class MyPPOTransformer(PPOModelForCausalLMWithValueHead, PPOModelLoss,SftWeightMinMax, with_pl=True):
+    def __init__(self, *args, **kwargs):
+        lora_args: LoraConfig = kwargs.pop('lora_args', None)
+        prompt_args: PromptLearningConfig = kwargs.pop('prompt_args', None)
+        ppo_args: PPOConfig = kwargs.pop('ppo_args', None)
+        super(MyPPOTransformer, self).__init__(*args, **kwargs)
+
+        self.lora_args = lora_args
+        self.ppo_config = ppo_args
+        self.prompt_args=prompt_args
+        if lora_args is not None and lora_args.with_lora:
+            self.backbone.enable_input_require_grads()
+            model: LoraModel = LoraModel(self.backbone, lora_args, auto_prepare_kbit_training=False)
+            print('==' * 30, 'lora info')
+            model.print_trainable_parameters()
+            self.set_model(model, copy_attr=False)
+            # for name, module in model.named_modules():
+            #     if isinstance(module, LoraLayer):
+            #         module = module.to(torch.bfloat16)
+            #     if 'norm' in name:
+            #         module = module.to(torch.float32)
+            #     if 'lm_head' in name or 'embed_tokens' in name:
+            #         if hasattr(module, 'weight'):
+            #             if module.weight.dtype == torch.float32:
+            #                 module = module.to(torch.bfloat16)
+
+    def get_model_lr(self, model=None, lr=None):
+        # for n, p in self.named_parameters():
+        #     print(n, p.requires_grad)
+        lr = lr if lr is not None else self.config.task_specific_params['learning_rate']
+        if self.lora_args is not None and self.lora_args.with_lora:
+            return [(self.backbone, lr)]
+        elif self.prompt_args and self.prompt_args.with_prompt:
+            return [(self.backbone, lr)]
+        return super(MyPPOTransformer, self).get_model_lr(model, lr)
+
+    def get_llm_model(self) -> PreTrainedModel:
+        if self.lora_args is not None and self.lora_args.with_lora:
+            return self.backbone.model.model
+        elif self.prompt_args is not None and self.prompt_args.with_prompt:
+            # PromptModel 方法覆盖原来方法
+            return self.backbone
+        return self.backbone.model
+
+    @torch.no_grad()
+    def generate(self,*args,**kwargs):
+        return self.get_llm_model().generate(*args,**kwargs)
+
+    def configure_optimizers(self):
+        p = self.get_named_parameters(self.backbone)
+        training_args = self.training_args
+        optimizer = AdamW(p, lr=training_args.learning_rate,
+                          eps=training_args.adam_epsilon,
+                          betas=training_args.optimizer_betas,
+                          weight_decay=training_args.weight_decay)
+        return optimizer
+
+
+    def training_step(self,*args, **inputs):
+        outputs = self.compute_loss(*args, **inputs)
+        return outputs
+
+    def validation_step(self, batch):
+        outputs = self.compute_loss(**batch)
+        return outputs
+
+    def compute_loss(self, *args, **inputs):
+        return self.forward_ppo_loss(*args, **inputs)
+
+
+    def forward_logits_values(self,*args,**kwargs):
+        return self.model.forward(*args,**kwargs)
+
+
+
+
+
+class Generate:
+    @classmethod
+    @torch.no_grad()
+    def generate(cls,model, tokenizer, query: str, max_length: int = 2048, num_beams=1,
+             do_sample=True, top_p=0.7, temperature=0.95, logits_processor=None, **kwargs):
+        gen_kwargs = {"max_length": max_length, "num_beams": num_beams, "do_sample": do_sample, "top_p": top_p,
+                      "temperature": temperature, "logits_processor": logits_processor, **kwargs}
+
+        # prompt = "Human：" + query + "\nAssistant："
+        #自行加模板
+        prompt = query
+        inputs = tokenizer([prompt], return_tensors="pt")
+        inputs = inputs.to(model.device)
+        outputs = model.generate(**inputs, **gen_kwargs)
+        outputs = outputs.tolist()[0][len(inputs["input_ids"][0]):]
+        response = tokenizer.decode(outputs)
+        return response
+
+    @classmethod
+    @torch.no_grad()
+    def chat(cls,model, tokenizer, query: str, history: List[Tuple[str, str]] = None, max_length: int = 2048, num_beams=1,
+             do_sample=True, top_p=0.7, temperature=0.95, logits_processor=None, **kwargs):
+        if history is None:
+            history = []
+
+        gen_kwargs = {"max_length": max_length, "num_beams": num_beams, "do_sample": do_sample, "top_p": top_p,
+                      "temperature": temperature, "logits_processor": logits_processor, **kwargs}
+        if not history:
+            prompt = query
+        else:
+            prompt = ""
+            for i, (old_query, response) in enumerate(history):
+                prompt += "[Round {}]\n问：{}\n答：{}\n".format(i, old_query, response)
+            prompt += "[Round {}]\n问：{}\n答：".format(len(history), query)
+        inputs = tokenizer([prompt], return_tensors="pt")
+        inputs = inputs.to(model.device)
+        outputs = model.generate(**inputs, **gen_kwargs)
+        outputs = outputs.tolist()[0][len(inputs["input_ids"][0]):]
+        response = tokenizer.decode(outputs)
+        history = history + [(query, response)]
+        return response, history
